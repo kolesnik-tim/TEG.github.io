e0b50f5985c77f6df733765be0a96d088661895e
Tim <ktn_96@icloud.com>
2019-04-10 18:21:30 +0300

    fix

--- src/js/import/admin-home.js
+++ src/js/import/admin-home.js
@@ -12,3 +12,14 @@ $(document).mouseup(function(e) {
 $('.dropdown-open').on('click', function() {
   $(this).siblings('.admin-home__header__notifications__drop-down, .admin-home__header__user__drop-down').fadeIn();
 });
+
+//aside open
+$('.aside').hover(function() {
+  $(this).addClass('active');
+}, function() {
+  $(this).removeClass('active');
+});
+$('.aside__menu__item--drop-down').on('click', function() {
+  $(this).toggleClass('active');
+  $(this).siblings('ul').slideToggle();
+});
