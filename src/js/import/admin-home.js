27a8ad4f0dd6acee2bc14ff9081e497d1776488a
Tim <ktn_96@icloud.com>
2019-04-23 10:06:48 +0300

    fix

--- src/js/import/admin-home.js
+++ src/js/import/admin-home.js
@@ -25,11 +25,3 @@ $('.aside__menu__item--drop-down').on('click', function() {
   $(this).toggleClass('active');
   $(this).siblings('ul').slideToggle();
 });
-$(document).mouseup(function(e) {
-  var container = $('.aside');
-  if (container.has(e.target).length === 0) {
-    container.removeClass('active');
-    $('.aside__menu__item--drop-down').removeClass('active');
-    $('.aside__menu__item--drop-down').siblings('ul').slideUp();
-  }
-});
